d49e5fca617cd349871c8c9f9cdceb58eca2be57
Emmanuel Kwaku Botchway <emmlquake@live.com>
2024-09-26 14:46:49 +0000

    Update: sw cache time

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 
 const CACHE_NAME = `sales_app_v${self.__APP_VERSION__ || '1.0.0'}`; // Versioned cache
-const CACHE_EXPIRATION_MS = 5 * 60 * 1000; // 5 minutes for static content
+const CACHE_EXPIRATION_MS = 3 * 60 * 1000; // 3 minutes for static content
 const DYNAMIC_CACHE_EXPIRATION_MS = 1 * 60 * 1000; // 1 minute for dynamic content
 const CACHE_ITEM_LIMIT = 30; // Limit of 30 cached items
 const FETCH_TIMEOUT = 5000; // 5 seconds timeout for network requests
